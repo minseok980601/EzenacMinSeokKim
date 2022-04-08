472898e99fe5bd67a087a5116ae3a6bb01310af5
minseok980601 <minseok980601@gmail.com>
2022-04-08 17:03:06 +0900

    "first commit"

--- EzenMinSeokKim2/src/kr/co/ezenac/git/ezenMinSeokKimExample.java
+++ EzenMinSeokKim2/src/kr/co/ezenac/git/ezenMinSeokKimExample.java
@@ -3,5 +3,7 @@ package kr.co.ezenac.git;
 public class ezenMinSeokKimExample {
 	public static void main(String[] args) {
 		System.out.println("new Git");
+		System.out.println("hahaha");
+		System.out.println("First Commit");
 	}
 }
